11ea00c71756b1c397f9018693966ef8b564db69
Muhammad Fauzan Fahmuddin <mfahmuddin4@gmail.com>
2019-03-09 09:56:49 +0700

    Fix : Detail User

--- app/src/main/java/com/dscutama/readapi/features/detail/DetailActivity.java
+++ app/src/main/java/com/dscutama/readapi/features/detail/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.dscutama.readapi.features.detail;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -22,6 +23,7 @@ public class DetailActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TextView txtAddress, txtPhone, txtNis;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
